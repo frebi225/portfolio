b4b9f974a97bc729c76e8797c3fba1972b5e112f
komaudrey <audreyfrebi900@gmail.com>
2025-03-31 16:22:16 +0000

    3eme version

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
             <div className="md:w-1/2 flex justify-center">
               <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white/20">
                 <Image
-                  src="/placeholder.svg?height=320&width=320"
+                  src="/public/placeholder.svg?height=320&width=320"
                   alt="Portrait"
                   fill
                   className="object-cover"
