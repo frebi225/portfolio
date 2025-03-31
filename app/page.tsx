a2153ce99b50b08b400b57ba3615845da6975e0b
komaudrey <audreyfrebi900@gmail.com>
2025-03-31 17:24:51 +0000

    4eme version

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
             <div className="md:w-1/2 flex justify-center">
               <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white/20">
                 <Image
-                  src="/moi.png"
+                  src="/moi.png?height=200&width=300"
                   alt="Portrait"
                   fill
                   className="object-cover"
